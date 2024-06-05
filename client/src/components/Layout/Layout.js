1273629e0832d1dd595fc7d9baac7ad683de5fea
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-05 17:50:37 +0530

    implementing basic SEO search

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Helmet } from "react-helmet";
 
-const Layout = (props) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-      <Header/>
-      <main style={{minHeight:'70vh'}}>
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
 
-        {props.children}
+      <Header />
+      <main style={{ minHeight: '70vh' }}>
+        {children}
       </main>
       <Footer />
     </div>
-  )
+  );
+};
+
+Layout.defaultProps ={ 
+  title:'E-Commerse app - Shop Now',
+  description: 'React MERN Project',
+  keywords:'mern,react,ecommerce,node,mongoDB',
+  author:'Anuraj Venkatpurwar',
 }
 
-export default Layout
+export default Layout;
---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Register = () => {
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [phone,setPhone] = useState("");
+    const [address,setAddress] = useState("");
+
+    return (
+        <Layout title={'Register Now'}>
+            <div className='register'>
+                <h1>Register Now</h1>
+                <form>
+                    <div className="mb-3">
+                        <input type="text" value={name} className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="email" value={email} className="form-control" id="exampleInputEmail1" placeholder="Enter Your Email" />
+                    </div>
+                    
+                    <div className="mb-3">
+                        <input type="password" value={password} className="form-control" id="exampleInputPassword1" placeholder="Enter Your Password" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" value={phone} className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone" />
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" value={address} className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address" />
+                    </div>
+                    
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default Register
