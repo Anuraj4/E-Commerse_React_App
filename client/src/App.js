7414f84a584e4d555c7c150a0f129316a4d04843
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-07 23:09:56 +0530

    implemented private routing, outlet, Loading in dashboard functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,17 @@ import Contact from './pages/Contact';
 import Register from './pages/Auth/Register';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Auth/Login';
+import Dashboard from './pages/Auth/Dashboard';
+import PrivateRoute from './components/Routes/Private';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/dashboard' element={<PrivateRoute/>}>
+          <Route path='' element={<Dashboard />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,12 +1,17 @@
 import { useState, useEffect, useContext, createContext } from 'react'
+import axios from 'axios'
 
 const AuthContext = createContext()
-
 const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({
         user: null,
         token: ""
     })
+    //default axios
+
+    axios.defaults.headers.common['Authorization'] = auth?.token
+
+
     useEffect(() => {
         const data = localStorage.getItem('auth')
         if(data){
@@ -17,8 +22,9 @@ const AuthProvider = ({ children }) => {
                 token:parseData.token
             })
         }
+        //eslint-disable-next-line
 
-    }, [auth]);
+    }, []);
     return (
         <AuthContext.Provider value={[auth, setAuth]}>
             {children}
