ce1b3aa00fbd9b7ea0f077b1c905998ed637f769
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-11 14:48:25 +0530

    implemented successfully product schema using mongoose, product route, product model, product controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,16 +1,17 @@
 import slugify from 'slugify';
 import categoryModel from '../models/categoryModel.js';
 
+// Create a new category
 export const createCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
     if (!name) {
-      return res.status(401).send({ message: 'Name is require' });
+      return res.status(400).send({ message: 'Name is required' });
     }
     const existingCategory = await categoryModel.findOne({ name });
     if (existingCategory) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: 'Category already exists'
       });
     }
@@ -25,17 +26,21 @@ export const createCategoryController = async (req, res) => {
     res.status(500).send({
       success: false,
       error,
-      message: 'Error in category'
+      message: 'Error in category creation'
     });
   }
 };
 
-// update category
+// Update an existing category
 export const updateCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
     const { id } = req.params;
-    const category = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) }, { new: true });
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
     res.status(200).send({
       success: true,
       message: 'Category updated successfully',
@@ -47,13 +52,11 @@ export const updateCategoryController = async (req, res) => {
       success: false,
       error,
       message: 'Error while updating category'
-
     });
   }
 };
 
-// get all category
-
+// Get all categories
 export const categoryController = async (req, res) => {
   try {
     const category = await categoryModel.find({});
@@ -65,14 +68,14 @@ export const categoryController = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({
-      success: true,
+      success: false,
       error,
       message: 'Error while getting all categories'
     });
   }
 };
 
-// single category
+// Get a single category by slug
 export const singleCategoryController = async (req, res) => {
   try {
     const category = await categoryModel.findOne({ slug: req.params.slug });
@@ -91,7 +94,7 @@ export const singleCategoryController = async (req, res) => {
   }
 };
 
-// delete category
+// Delete a category by ID
 export const deleteCategoryController = async (req, res) => {
   try {
     const { id } = req.params;
